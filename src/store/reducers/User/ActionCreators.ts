f248a9f388f5b270ae48802bc805c4714116b8b6
Dzmitry Viartseika <dzmitry_viartseika@senla.eu>
2022-05-19 16:28:14 +0300

    redux toolkit

--- src/store/reducers/User/ActionCreators.ts
+++ src/store/reducers/User/ActionCreators.ts
@@ -22,7 +22,7 @@ export const fetchUsers = createAsyncThunk(
     'user/fetchAll',
     async (_, thunkApi) => {
         try {
-            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users1');
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
             return response.data;
         } catch (e) {
             console.error(e);
